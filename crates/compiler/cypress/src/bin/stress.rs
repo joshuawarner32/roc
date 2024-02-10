c62fdd5910e16b9f64598d64af4e7bc2ea9b48a4
Joshua Warner <joshuawarner32@gmail.com>
2024-02-09 18:59:36 -0800

    stress

---
+++ crates/compiler/cypress/src/bin/stress.rs
@@ -0,0 +1,53 @@
+use std::panic::catch_unwind;
+
+use bumpalo::Bump;
+use roc_cypress::parse::State;
+use roc_cypress::token::Tokenizer;
+use roc_parse::parser::Parser;
+
+const DATA: &str = include_str!("../../../../../crates/glue/src/RustGlue.roc");
+
+fn main() {
+    let start = std::time::Instant::now();
+
+    while start.elapsed().as_secs() < 10 {
+        parse_once(&DATA);
+    }
+}
+
+fn parse_once(text: &str) -> Option<String> {
+    let mut tokenizer = Tokenizer::new(text);
+    tokenizer.tokenize();
+    let (messages, tb) = tokenizer.finish();
+    assert_eq!(messages, vec![]);
+    // eprint!("tokens:");
+    // let mut last = 0;
+    // for (i, (begin, indent)) in tb.lines.iter().enumerate() {
+    //     for tok in &tb.kinds[last as usize .. *begin as usize] {
+    //         eprint!(" {:?}", tok);
+    //     }
+    //     eprint!("\n{}: {:?} {}.{}:", i, begin, indent.num_spaces, indent.num_tabs);
+    //     last = *begin;
+    // }
+    // for tok in &tb.kinds[last as usize..] {
+    //     eprint!(" {:?}", tok);
+    // }
+    // eprintln!();
+
+    let mut state = State::from_buf(tb);
+    state.start_file();
+    state.pump();
+    state.assert_end();
+
+    if let Some(msg) = state.messages.first() {
+        let top_frames = msg.frames.iter().rev().take(2);
+        let formatted_frames = top_frames
+            .map(|frame| format!("{:?}", frame))
+            .collect::<Vec<_>>()
+            .join(" -> ");
+
+        Some(format!("{:?}: at {}", msg.kind, formatted_frames))
+    } else {
+        None
+    }
+}
